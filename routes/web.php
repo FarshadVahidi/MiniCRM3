85aadd1dd29e8b2f1d083140db7efb9ca8a123d9
farshad vahidifard <farshad.vahidifard@studenti.unimi.it>
2021-03-22 13:36:12 +0100

    web.php add Route::group for all type of roles

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,21 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::group(['middleware' => 'auth'], function(){
+
+   Route::group(['middleware' => 'role:user', 'prefix' => 'user', 'as' => 'users.'], function() {
+       Route::resource('user', App\Http\Controllers\User\UserController::class);
+       Route::resource('company', App\Http\Controllers\User\CompanyController::class);
+   });
+
+    Route::group(['middleware' => 'role:administrator', 'prefix' => 'admin', 'as' => 'admins.'], function(){
+        Route::resource('user', App\Http\Controllers\Admin\AdminController::class);
+        Route::resource('company', App\Http\Controllers\Admin\CompanyController::class);
+    });
+
+    Route::group(['middleware' => 'role:superadministrator', 'prefix' => 'super', 'as' => 'supers.'], function(){
+        Route::resource('user', App\Http\Controllers\Super\SuperController::class);
+        Route::resource('company' , App\Http\Controllers\Super\CompanyController::class);
+    });
+});
