b8523ffe52823bd232c34e0b9196bd9457085149
farshad vahidifard <farshad.vahidifard@studenti.unimi.it>
2021-03-23 11:34:37 +0100

    admin can see his/her company detail and edit and update

--- app/Http/Controllers/Admin/CompanyController.php
+++ app/Http/Controllers/Admin/CompanyController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Company;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\View;
 
 class CompanyController extends Controller
 {
@@ -42,22 +46,29 @@ class CompanyController extends Controller
      * Display the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\View\View
      */
     public function show($id)
     {
-        //
+        if($id != auth()->user()->company_id)
+            abort(403);
+
+        $company = Company::findOrFail($id);
+        return View::make('Admin.company.show', compact('company'));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\View\View
      */
-    public function edit($id)
+    public function edit(Company $company)
     {
-        //
+        $user = auth()->user();
+        if($user->company_id != $company->id)
+            abort(403);
+        return View::make('Admin.company.edit', compact('company'));
     }
 
     /**
@@ -65,11 +76,20 @@ class CompanyController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\View\View
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Company $company)
     {
-        //
+        try{
+            $company->update($this->validateRequest());
+            $this->storeImage($company);
+
+            Session::flash('message', 'Data Base Successfully Updated.');
+            return View::make('User.company.edit', compact('company'));
+        }catch(Exception $e){
+            Session::flash('alert', 'There Is Problem, Update Aborted.');
+            return View::make('User.company.edit', compact('company'));
+        }
     }
 
     /**
@@ -81,5 +101,33 @@ class CompanyController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    private function validateRequest()
+    {
+        return tap(request()->validate([
+            'name'  => 'required|string|min:3|max:255',
+            'email' => 'required|email',
+            'website'   => ['required', 'regex:/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i'],
+        ]), function (){
+            if(request()->hasFile('photo')){
+                if(request()->hasFile('photo')){
+                    request()->validate([
+                        'photo' => 'file|image|max:5000'
+                    ]);
+                }
+            }
+        });
+    }
+
+    //STILL THE SAME ERROR!!!
+    private function storeImage($company)
+    {
+        if(request()->hasFile('photo'))
+        {
+            $company->update([
+                'photo'  => request()->photo->store('uploads', 'public'),
+            ]);
+        }
     }
 }
---
+++ resources/views/Admin/company/show.blade.php
@@ -0,0 +1,47 @@
+<x-app-layout>
+    <x-slot name="header">
+        <div class="row">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('Company Profile') }}
+            </h2>
+        </div>
+    </x-slot>
+
+    @section('message')
+        {{ Session::get('message') }}
+    @endsection
+    @section('alert')
+        {{ Session::get('alert') }}
+    @endsection
+
+    @section('mainContent')
+
+        <div class="row">
+            {{ __('Detail: ') }}
+            {{ $company->name }}
+        </div>
+        <br>
+
+        <div class="row mt-3">
+            <div class="col-12">
+                <p><strong>{{ __('First Name: ') }}</strong> {{ $company->name }}</p>
+                <p><strong>{{ __('Email: ') }}</strong> {{ $company->email }}</p>
+                <p><strong>{{ __('Website: ') }}</strong> {{ $company->website }}</p>
+            </div>
+        </div>
+
+        @if($company->photo)
+            <div class="row">
+                <div class="col-12">
+                    <img src="{{ asset('storage/image/'. $company->photo) }}" alt="{{ __('Company Logo') }}" class="img-thumbnail">
+                </div>
+            </div>
+        @endif
+
+        @can('companies-update')
+            <div class="col-12 mt-3">
+                <a type="button" class="btn btn-success" href="{{ route('admins.company.edit', $company->id) }}">{{ __('Edit') }}</a>
+            </div>
+        @endif
+    @endsection
+</x-app-layout>
